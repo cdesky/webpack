ab5dba9853f83df3c7c13b400346eac3dcf4f710
冬哥 <1724024479@qq.com>
2019-03-04 17:02:44 +0800

    行拖动

--- src/router/routerMap.js
+++ src/router/routerMap.js
@@ -5,6 +5,7 @@ export default {
   app: Ac(() => import("component/Body/index")),
   project: Ac(() => import("pages/project/project")),
   site: Ac(() => import("pages/site/site")),
+  dragTr: Ac(() => import("pages/site/dragTr")),
   projectDetail: Ac(() => import("pages/project/view/projectDetail")),
   table: Ac(() => import("pages/project/view/projectTb"))
 };
